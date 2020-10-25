2d31f1e4419dbf2b6d5c03fae0d6f5cac6242a16
vimalmv91 <72956030+vimalmv91@users.noreply.github.com>
2020-10-25 22:23:51 +0530

    Add files via upload

--- src/main/java/com/vim/test/controller/BlogController.java
+++ src/main/java/com/vim/test/controller/BlogController.java
@@ -5,17 +5,18 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.vim.test.entity.Blog;
 import com.vim.test.service.BlogServiceImpl;
 import com.vim.test.vo.BlogVO;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/blog")
 public class BlogController {
 	private final Logger LOG = LoggerFactory.getLogger(getClass());
